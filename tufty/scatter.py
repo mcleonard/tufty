e508c911727de4deeaad2ea53681442c851168ba
Mathew Leonard <leonard.mat@gmail.com>
2019-07-24 10:07:36 -0700

    Add scatter plot color by numerical feature

--- tufty/scatter.py
+++ tufty/scatter.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 class ScatterMixin:
             
     def scatter(self, x=None, y=None, size=None, color=None):
         if color in self.data.columns:
-            for group, group_data in self.data.groupby(color):
-                self.ax.scatter(group_data[x], group_data[y], s=size, label=group)
-                self.ax.set_xlabel(x)
-                self.ax.set_ylabel(y)
-        
+            series =  self.data[color]
+            if series.dtype == np.dtype("O"):
+                for group, group_data in self.data.groupby(color):
+                    self.ax.scatter(group_data[x], group_data[y], s=size, label=group)
+                    self.ax.set_xlabel(x)
+                    self.ax.set_ylabel(y)
+            else:
+                self.ax.scatter(self.data[x], self.data[y], s=size, c=self.data[color])
+
         else:
             self.ax.scatter(self.data[x], self.data[y], s=size, c=color)
             self.ax.set_xlabel(x)
